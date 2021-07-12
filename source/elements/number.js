6b05d2dfd83d18df899c7ff58288e91990ad0077
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:55:56 -0400

    update build

--- source/elements/number.js
+++ source/elements/number.js
@@ -1,5 +1,9 @@
+import number from '../templates/number.js';
+
 export class Number extends Quantum {
     constructor() {
         super();
     }
 }
+
+Number.define('quantum-number', number);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Number } from './elements/number.js';
-import number from './templates/number.js';
-
-Number.define('quantum-number', number);
